0a2107a57fc38b08bcc8a4462d628140d0af4fd7
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 00:39:04 +0100

    try

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,4 @@
 export default function divideFunction(numerator, denominator) {
-    if (denominator === 0) {
-        throw Error('cannot divide by 0');
-    } else {
-        return numerator / denominator;
-    }
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  return numerator / denominator;
 }
