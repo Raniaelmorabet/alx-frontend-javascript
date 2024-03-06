8a95073ea4ff77cc3d7cc237843222842b46d290
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 00:43:31 +0100

    block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    return [task, task2];
   }
-  /* eslint-enable no-unused-vars */
-
   return [task, task2];
 }
