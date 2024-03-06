fc9dc7f913d1870206d176891fba411a80ff1a06
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 00:36:34 +0100

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise
-        .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-        .then((res) => (
-            res.map((o) => ({
-                status: o.status,
-                value: o.status === 'fulfilled' ? o.value : String(o.reason),
-            }))
-        ));
+export default function handleProfileSignup(firstName, lastName, photo) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(photo)])
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason.toString(),
+    })));
 }
