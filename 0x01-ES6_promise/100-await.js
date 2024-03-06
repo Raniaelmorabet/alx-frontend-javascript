f80102674e8e0a4709dd0cad869c8cb2fdd82044
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 00:32:34 +0100

    await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    let res = {};
-
-    try {
-        const photo = await uploadPhoto();
-        const user = await createUser();
-        res = { photo, user };
-    } catch (err) {
-        res = { photo: null, user: null };
-    }
-    return res;
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
 }
