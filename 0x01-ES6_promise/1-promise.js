4d9fe9ca87a10d80c72a510f9866917767ef3347
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 00:30:12 +0100

    promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,4 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success',
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+  if (success) return Promise.resolve({ status: 200, body: 'Success' });
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
