8ab3db4c8b71544747860820d11a49c511f3d45f
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 23:36:01 +0100

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,26 +1,26 @@
 export default class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
 
-    set code(code) {
-        this._code = code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    get code() {
-        return this._code;
-    }
+  set code(code) {
+    this._code = code;
+  }
 
-    set name(name) {
-        this._name = name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    get name() {
-        return this._name;
-    }
+  set name(name) {
+    this._name = name;
+  }
 
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
-    }
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
