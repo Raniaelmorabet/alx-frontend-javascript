a4e4aae23ce4cff712cd3a5c10ab793dadb897d3
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 23:37:57 +0100

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,16 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        this._size = size;
-        this._location = location;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-    set size(size) {
-        this._size = size;
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
     }
-
-    get size() {
-        return this._size;
+    if (hint === 'string') {
+      return this._location;
     }
-
-    set location(location) {
-        this._location = location;
-    }
-
-    get location() {
-        return this._location;
-    }
-
-    [Symbol.toPrimitive](hint) {
-        if (hint === 'string') {
-            return this.location;
-        }
-        if (hint === 'number') {
-            return this.size;
-        }
-        return null;
-    }
+    return null;
+  }
 }
