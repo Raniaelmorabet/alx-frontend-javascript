59f15e3095c12398b7177ea0d365613d3a78e88b
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 23:38:57 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,15 +1,16 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand = null, motor = null, color = null) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-    static get [Symbol.species]() {
-        return this;
-    }
-
-    cloneCar() {
-        return new this.constructor[Symbol.species]();
-    }
+  cloneCar() {
+    const clone = new this.constructor();
+    const symbols = Object.getOwnPropertySymbols(this);
+    symbols.forEach((symbol) => {
+      clone[symbol] = this[symbol];
+    });
+    return clone;
+  }
 }
