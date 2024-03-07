7297c8b4aa9589c5730a60bf8d2428accd2b80dd
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 23:36:27 +0100

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,33 +1,30 @@
-// eslint-disable-next-line
-import Currency from './3-currency'
-
 export default class Pricing {
-    constructor(amount, currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    set amount(amount) {
-        this._amount = amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  set amount(amount) {
+    this._amount = amount;
+  }
 
-    set currency(currency) {
-        this._currency = currency;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    get currency() {
-        return this._currency;
-    }
+  set currency(currency) {
+    this._currency = currency;
+  }
 
-    displayFullPrice() {
-        return `${this.amount} ${this.currency.displayFullCurrency()}`;
-    }
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
