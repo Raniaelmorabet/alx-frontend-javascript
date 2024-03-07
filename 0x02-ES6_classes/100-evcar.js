07cf0b6de9b9a9b7c0b1d6dfebbb9f58f1f61efc
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 23:39:14 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,12 +1,12 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this._range = range;
-    }
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-    static get [Symbol.species]() {
-        return Car;
-    }
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
 }
