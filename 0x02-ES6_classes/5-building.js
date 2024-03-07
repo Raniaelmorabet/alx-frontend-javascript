7de7bb297e0d8e42e1502fad613b37966964af0b
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-03-07 23:36:44 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-        this._sqft = sqft;
-        if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+  constructor(sqft) {
+    if (new.target !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    set sqft(value) {
-        this._sqft = value;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  set sqft(value) {
+    this._sqft = value;
+  }
 }
